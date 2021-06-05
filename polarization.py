6b3be50a0fe6fbce4617a878eb1141bbf2e72852
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-06-05 10:51:27 +0300

    python for part A

---
+++ polarization.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+Part A section C
+"""
+def fieldDirection(t, w):
+    #exp = math.e**j(w*t-)
+    #Ep = 4*(math.e**)
+
+"""
+Part A section D
+"""    
